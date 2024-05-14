e6b0b8c3311bbdb2dd14cc0c70e4edb3910d79d8
McMmie <tharausamuel01@gmail.com>
2024-05-14 00:22:22 +0000

    database: created a file that creates a client to mongodb

--- utils/db.js
+++ utils/db.js
@@ -1,56 +1,56 @@
 import mongodb from 'mongodb';
-import envLoader from './env';
+// eslint-disable-next-line no-unused-vars
 import collection from 'mongodb/lib/collection';
+import envLoader from './env';
 
 /*
  * creates a client to mongodb
  */
 
 class DBClient {
-	/*
-	 * creates a client
-	 */
+  /*
+   * creates a client
+   */
 
-	constructor() {
-		envLoader();
-		const host = process.env.DB_HOST || 'localhost';
-		const port = process.env.DB_PORT || 27017;
-		const database = process.env.DB_DATABASE || 'files_manager';
-		const dburl = `mongodb://${host}:${port}/${database}`;
+  constructor() {
+    envLoader();
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+    const dburl = `mongodb://${host}:${port}/${database}`;
 
-		this.client = new mongodb.MongoClient(dburl, { useUnifiedTopology: true });
-		// this.client.connect();
+    this.client = new mongodb.MongoClient(dburl, { useUnifiedTopology: true });
+    // this.client.connect();
+  }
 
-	}
-	/*
-	 * returns true when connection is successful
-	 */
-	isAlive() {
+  /*
+   * returns true when connection is successful
+   */
+  isAlive() {
+    try {
+      this.client.connect();
+      return true;
+    } catch (err) {
+      return false;
+    }
+  }
 
-		try {
-			this.client.connect();
-			return true;
-		} catch (err) {
-			return false;
-		}
-	}
+  /*
+   * returns the number of documents in the collection users
+   */
 
-	/*
-	 * returns the number of documents in the collection users
-	 */
+  async nbUsers() {
+    return this.client.db().collection('users').countDocuments();
+  }
 
-	async nbUsers() {
-		return this.client.db().collection('users').countDocuments();
-	}
+  /*
+   * returns the number of documents in the collection files
+   */
 
-	/*
-	 * returns the number of documents in the collection files
-	 */
-
-	async nbFiles () {
-		return this.client.db().collection('files').countDocuments();
-	}
+  async nbFiles() {
+    return this.client.db().collection('files').countDocuments();
+  }
 }
 
-	export const dbClient = new DBClient();
-	export default dbClient;
+export const dbClient = new DBClient();
+export default dbClient;
